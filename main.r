4ee7693e52340bd63e4acaab7d8f93744eeb36d6
Tanja Nuendel <tanja.nuendel@student.fh-kiel.de>
2019-11-07 10:43:37 -0800

    Initial commit

---
+++ main.r
@@ -0,0 +1,15 @@
+# Blackjack Game
+
+# Deck simulation (Tanja)
+
+
+# Dealer's game - hardfast rule
+
+# Player strategies (Mushahid)
+
+# Func: drawing without replacement
+# use sample
+
+# shuffling
+
+# counting for winning
