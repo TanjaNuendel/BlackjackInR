474f3b3dac37ba7481afc671c5fb78863fc2f6de
Mushahid Hassan <31359447+mushi14@users.noreply.github.com>
2019-11-12 18:16:32 -0800

    Update main.r

--- main.r
+++ main.r
@@ -28,7 +28,10 @@ deck <- data.frame(cardtype, cardvalue, current_deckfreq, stringsAsFactors = TRU
 
 # Player strategies (Mushahid)
 players <- list()
-playersDF <- data.frame(cards = vector(), numericValues = vector(), total = vector())
+playersDF <- data.frame(cards = character(), numericValues = vector(), total = vector(), 
+                        status = character())
+
+printf <- function(...) cat(sprintf(...))
 
 dfPopulate <- function(numPlayers) {
   for (i in 1:numPlayers) {
@@ -37,51 +40,72 @@ dfPopulate <- function(numPlayers) {
   return(players)
 }
 
-# win <- function() {
-#   
-# }
-# 
-# gameEnd <- function() {
-#   if (win) {
-#     return(TRUE)
-#   } else {
-#     return(FALSE)
-#   }
-# }
-
-#Updates the data frame for the player, given the player number, card type, and card number
-updateTotal <- function(player_num, card_type, card_val) {
-  typeVec <- append(players[[player_num]][, 1], card_type)
-  valVec <- append(players[[player_num]][, 2], card_val)
-  
-  len <- length(players[[player_num]][, 3])
-  if (len == 0) {
-    totalVec <- append(players[[player_num]][, 3], card_val)
+CheckStatus <- function(player_num, total) {
+  status_vec <- vector()
+  if (total == 21) {
+    status_vec <- append(as.character(players[[player_num]][, 4]), "won")
+  } else if (total > 21) { 
+    status_vec <- append(as.character(players[[player_num]][, 4]), "lost")
+  } else if (total >= 17 &&  total < 21) {
+    status_vec <- append(as.character(players[[player_num]][, 4]), "standing")
   } else {
-    total = card_val + players[[player_num]][, 3][len]
-    totalVec <- append(players[[player_num]][, 3], total)
+    status_vec <- append(as.character(players[[player_num]][, 4]), "hitting")
   }
-
-  players[[player_num]] <- data.frame(cards = typeVec, numericValues = valVec, total = totalVec)
-  return(players)
+  
+  return(status_vec)
 }
 
 #Basic way to determine whether to hit or stand
-HitOrStand <- function(player_num) {
-  total <- players[[player_num]][, 3][length(players[[player_num]][, 3])]
-  if (total >= 17) {
-    print("Stand")
+HitOrStand <- function(player_num, vec) {
+  total <- vec[length(vec)]
+  if (total == 21) {
+    printf("Player %d: WON!\n", player_num)
+    CheckStatus(player_num, total)
+  } else if (total > 21) {
+    printf("Player %d: BUSTED!\n", player_num)
+    CheckStatus(player_num, total)
+  } else if (total >= 17 && total < 21) {
+    printf("Player %d: Stand\n", player_num)
+    CheckStatus(player_num, total)
   } else {
-    print("Hit")
+    printf("Player %d: Hit\n", player_num)
+    CheckStatus(player_num, total)
   }
 }
 
+#This is the function the dealer will call after checking if the player wants to hit or
+#stand. The dealer will pass in the player number, the type of card drawn, and the numeric 
+#value of that card. From there, the data frame for the player will be updated. 
+updateTotal <- function(player_num, card_type, card_val) {
+  typeVec <- append(as.character(players[[player_num]][, 1]), card_type)
+  valVec <- append(players[[player_num]][, 2], card_val)
+  
+  len <- length(players[[player_num]][, 3])
+  status_vec <- vector()
+  
+  if (len == 0) {
+    totalVec <- append(players[[player_num]][, 3], card_val)
+    status_vec <- HitOrStand(player_num, totalVec)
+  } else {
+    total = card_val + players[[player_num]][, 3][len]
+    totalVec <- append(players[[player_num]][, 3], total)
+    
+    status_vec <- HitOrStand(player_num, totalVec)
+  }
+  
+  players[[player_num]] <- data.frame(cards = typeVec, numericValues = valVec, total = totalVec,
+                                      status = status_vec)
+  
+  return(players)
+}
+
+#The number of players n, must be passed in at the start
 players <- dfPopulate(4)
 
 #Populating player 1
 players <- updateTotal(1, "Ace", 11)
 players <- updateTotal(1, "5", 5)
-players <- updateTotal(1, "King", 10)
+players <- updateTotal(1, "4", 4)
 
 players
 
