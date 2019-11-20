b8319fea2e2df737a681dd8408443decf0a29e8a
Conor Falvey <conorfalvey@Conors-MacBook-Pro.local>
2019-11-19 23:43:48 -0800

    Done

--- main.r
+++ main.r
@@ -1,4 +1,8 @@
-# Blackjack Game
+#Tanja Neundel
+#Mushahid Hassan
+#Conor Falvey
+
+#Case Study 3
 
 # libraries
 library(dplyr)
@@ -11,6 +15,8 @@ deck_count <- 1
 total_cards <- deck_count * 52
 cardtype <- c("Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King")
 cardvalue <- c(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10)
+numPlayers <- 4
+finished = FALSE
 
 # make a vector of frequencies - in the beginning 4 cards per type and deck
 current_deckfreq <- rep(deck_count*4, 13)
@@ -21,42 +27,31 @@ card_probs <- rep(1/13, total_cards/4)
 deck <- data.frame(cardtype, cardvalue, current_deckfreq, card_probs, stringsAsFactors = TRUE)
 
 # keeps deck updated with card type frequencies and probablilities
-updateDeck <- function(deck, drawn_card) {
+updateDeck <- function(drawn_card) {
   # drawn card gets removed from deck
-  deck$current_deckfreq[which(cardtype == drawn_card)] <- (deck$current_deckfreq[which(cardtype == drawn_card)] - 1)
-  #probabilities are updated
-  drawn_card_index <- which(cardtype == drawn_card)
-  updateProbs <- function(){
-    deck$card_probs[index] <- deck$current_deckfreq[index] / sum(deck$current_deckfreq)
-  }
+  deck$current_deckfreq[which(deck$cardtype == as.factor(drawn_card))] <<- (deck$current_deckfreq[which(deck$cardtype == as.factor(drawn_card))] - 1)
   
-  # this throws an error: " Error in FUN(X[[i]], ...) : unused argument (arg1 = drawn_card_index)"
-  deck$card_probs <- lapply(deck$card_probs, 
-                            function(x) deck$card_probs[drawn_card_index] 
-                            <- deck$current_deckfreq[drawn_card_index] / 
-                              sum(deck$current_deckfreq))
+  #probabilities are updated
+  for (i in 1:length(deck$cardtype)) {
+    deck$card_probs[[i]] <<- deck$current_deckfreq[[i]] / colSums(deck[3])
+  }
 }
 
 # function to draw a card from the current deck
-drawCard <- function(x){
+drawCard <- function(){
   drawn_card <- sample(deck$cardtype, size = 1, replace = TRUE)
-  print(drawn_card)
   # if card is already out, draw again until there is one
   if (deck$current_deckfreq[which(cardtype == drawn_card)] == 0){
     drawn_card <- sample(deck$cardtype, size = 1, replace = TRUE)
   } else if(sum(deck$current_deckfreq) == 0) {
     # make new deck if all cards are empty
-    deck <- data.frame(cardtype, cardvalue, current_deckfreq, card_probs, stringsAsFactors = TRUE)
+    deck <<- data.frame(cardtype, cardvalue, current_deckfreq, card_probs, stringsAsFactors = TRUE)
   } else {
-    updateDeck(deck, drawn_card)
+    updateDeck(drawn_card)
   }
+  return(drawn_card)
 }
 
-drawCard()
-
-# Dealer's game - hardfast rule
-
-# Player strategies (Mushahid)
 players <- list()
 playersDF <- data.frame(cards = character(), numericValues = vector(), total = vector(), 
                         status = character())
@@ -74,6 +69,7 @@ CheckStatus <- function(player_num, total) {
   status_vec <- vector()
   if (total == 21) {
     status_vec <- append(as.character(players[[player_num]][, 4]), "won")
+    finished <- TRUE
   } else if (total > 21) { 
     status_vec <- append(as.character(players[[player_num]][, 4]), "lost")
   } else if (total >= 17 &&  total < 21) {
@@ -89,16 +85,16 @@ CheckStatus <- function(player_num, total) {
 HitOrStand <- function(player_num, vec) {
   total <- vec[length(vec)]
   if (total == 21) {
-    printf("Player %d: WON!\n", player_num)
+    #printf("Player %d: WON!\n", player_num)
     CheckStatus(player_num, total)
   } else if (total > 21) {
-    printf("Player %d: BUSTED!\n", player_num)
+    #printf("Player %d: BUSTED!\n", player_num)
     CheckStatus(player_num, total)
   } else if (total >= 17 && total < 21) {
-    printf("Player %d: Stand\n", player_num)
+    #printf("Player %d: Stand\n", player_num)
     CheckStatus(player_num, total)
   } else {
-    printf("Player %d: Hit\n", player_num)
+    #printf("Player %d: Hit\n", player_num)
     CheckStatus(player_num, total)
   }
 }
@@ -129,19 +125,94 @@ updateTotal <- function(player_num, card_type, card_val) {
   return(players)
 }
 
-#The number of players n, must be passed in at the start
-players <- dfPopulate(4)
+# Set the number of trials, decks, players, and shuffles to run in the simulation
+trials = 1:25
+decks = c(1, 2, 3, 4, 5)
+player = c(1, 2, 3, 4, 5)
+deck_shuffles = c(1, 2, 3, 4, 5)
 
-#Populating player 1
-players <- updateTotal(1, "Ace", 11)
-players <- updateTotal(1, "5", 5)
-players <- updateTotal(1, "4", 4)
+#Create list to add data to in each pass
+odds <- data.frame(matrix(ncol = 4, nrow = 0))
+colnames(odds) <- c("decks", "players", "shuffles", "wins")
 
-players
+for (curr_decks in decks) {
+  for (curr_players in player) {
+    for (curr_deck_shuffles in deck_shuffles) {
+      for (trial in trials) {
+        deck_count <- curr_decks
+        numPlayers <- curr_players
+        for (i in curr_deck_shuffles) {
+          deck <- data.frame(cardtype, cardvalue, current_deckfreq, card_probs, stringsAsFactors = TRUE)
+        }
+        
+        #The number of players n, must be passed in at the start
+        players <- dfPopulate(numPlayers)
+        
+        #Deal initial two cards to players
+        for (i in 1:length(players)) {
+          curr <- drawCard()
+          players <- updateTotal(i, curr, cardvalue[curr])
+          curr <- drawCard()
+          players <- updateTotal(i, curr, cardvalue[curr])
+        }
+        
+        #Deal dealers cards
+        dealer <- cardvalue[drawCard()] + cardvalue[drawCard()]
+        change <- TRUE
+        
+        #Main loop
+        #Finished - Checks if a player has won to end the loop
+        #Change - Detects change in player cards to see when players are done
+        while(!finished && change) {
+          change <- FALSE
+          for (i in 1:numPlayers) {
+            if (players[[i]]$total[length(players[[i]]$total)] == 21) {
+              finished <- TRUE
+              break
+            } else if (players[[i]]$total[length(players[[i]]$total)] <= 14) {
+              curr <- drawCard()
+              players <- updateTotal(i, curr, cardvalue[curr])
+              change <- TRUE
+            } else if (players[[i]]$total[length(players[[i]]$total)] > 21) {
+              players[[i]]$status = "lost"
+            }
+          }
+        }
+        
+        #Determine the player with the highest pair of legal cards
+        highest = list(0, 0)
+        for (i in 1:numPlayers) {
+          if (players[[i]]$total[length(players[[i]]$total)] > highest[2]) {
+            highest[1] = i
+            highest[2] = players[[i]]$total[length(players[[i]]$total)]
+          }
+        }
+        
+        #Make dealer draw until they hit 17 or beat the player with the highest score
+        while (dealer <= 17 && dealer < highest[2]) {
+          dealer <- dealer + cardvalue[drawCard()]
+          if (dealer <= 21 && !finished && dealer > highest[2]) {
+            #print("Dealer Won!")
+          } else if (dealer > 21) {
+            #print("Dealer Busted!")
+            #printf("Player %d Won!\n", as.numeric(highest[1]))
+          }
+        }
+        
+        #Add results to list
+        if (as.numeric(highest[1]) == 1){
+          odds <- rbind(odds, c(curr_decks, curr_players, curr_deck_shuffles, 1))
+        } else {
+          odds <- rbind(odds, c(curr_decks, curr_players, curr_deck_shuffles, 0))
+        }
+      }
+    }
+  }
+}
 
-# Func: drawing without replacement
-# use sample()
-# handle zero case: if current_deckfreq of a card is already 0, it will stay zero
-
-
-# counting for winning
+#Turn list into data frame
+data <- rbind.data.frame(odds)
+colnames(data) <- c("decks", "players", "shuffles", "win")
+#Filter the data for wins, and count those per each scenario
+test <- data %>% filter(win == 1) %>% count(decks, players, shuffles)
+print.data.frame(test)
